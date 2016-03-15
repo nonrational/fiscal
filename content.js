1f59dc0a50f1d11ace9dc838e278f114dcb7cb96
Alan Norton <me@alannorton.com>
2016-03-15 00:19:26 -0400

    in media res

--- content.js
+++ content.js
@@ -1,7 +1,60 @@
-// console.js
-console.log('fiscal v0.0.1')
+var FisCal = (function FisCal(){
+    var HOURLY_CONSTANT = 60
 
-var HOURLY_CONSTANT = 60
+    function arr(selector){
+        var elements = querySelectorAll(selector);
+        return elements && elements.length > 0 ? elements : [];
+    }
+
+    var page = {
+        startTime: arr("input[id$=st].text.dr-time")[0],
+        endTime: arr("input[id$=et].text.dr-time")[0],
+        guests: arr("div[id$=guests].ep-dp-guests div.ep-gl-guest")
+    }
+
+    // http://www.timlabonne.com/2013/07/parsing-a-time-string-with-javascript/
+    function parseTime(timeStr, dt) {
+        if(!timeStr){
+            return timeStr;
+        }
+
+        if (!dt) {
+            dt = new Date();
+        }
+
+        var time = timeStr.match(/(\d+)(?::(\d\d))?\s*(p?)/i);
+        if (!time) {
+            return NaN;
+        }
+        var hours = parseInt(time[1], 10);
+        if (hours == 12 && !time[3]) {
+            hours = 0;
+        }
+        else {
+            hours += (hours < 12 && time[3]) ? 12 : 0;
+        }
+
+        dt.setHours(hours);
+        dt.setMinutes(parseInt(time[2], 10) || 0);
+        dt.setSeconds(0, 0);
+        return dt;
+    }
+
+
+    function duration(){
+        var startDateTime = parseDateTime(page.star,
+            endDateTime = _endTime();
+
+    if(startDateTime === null || endDateTime === null) {
+        return 0;
+    }
+    return Math.abs(endDateTime - startDateTime) / 36e5;
+    }
+
+}());
+
+
+
 
 function appendFooter(cost){
     var descriptionTextArea = document.querySelectorAll("div.ui-sch > textarea");
@@ -31,45 +84,6 @@ function _duration(){
     return Math.abs(endDateTime - startDateTime) / 36e5;
 }
 
-function _guests(){
-    var guestList = document.querySelectorAll("div[id$=guests].ep-dp-guests div.ep-gl-guest")
-    return guestList ? guestList.length : 0;
-}
-
-function _startTime(){
-    var startInput = document.querySelectorAll("input[id$=st].text.dr-time")
-    return startInput && startInput.length > 0 ? parseTime(startInput[0].value) : null;
-}
-
-function _endTime(){
-    var endInput = document.querySelectorAll("input[id$=et].text.dr-time")
-    return endInput && endInput.length > 0 ? parseTime(endInput[0].value) : null;
-}
-
-// http://www.timlabonne.com/2013/07/parsing-a-time-string-with-javascript/
-function parseTime(timeStr, dt) {
-    if (!dt) {
-        dt = new Date();
-    }
-
-    var time = timeStr.match(/(\d+)(?::(\d\d))?\s*(p?)/i);
-    if (!time) {
-        return NaN;
-    }
-    var hours = parseInt(time[1], 10);
-    if (hours == 12 && !time[3]) {
-        hours = 0;
-    }
-    else {
-        hours += (hours < 12 && time[3]) ? 12 : 0;
-    }
-
-    dt.setHours(hours);
-    dt.setMinutes(parseInt(time[2], 10) || 0);
-    dt.setSeconds(0, 0);
-    return dt;
-}
-
 function announce(){
     var guests = _guests(),
         duration = _duration(),
@@ -81,4 +95,5 @@ function announce(){
     }
 }
 
+console.log('fiscal v0.0.1')
 setInterval(announce, 5000);
